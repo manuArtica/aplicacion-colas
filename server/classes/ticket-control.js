96b7356c004ff86ced1614a5020e5a38998a2428
Manuel Montes <aceptoelreto27@gmail.com>
2019-12-15 20:56:18 +0100

    Primer commit - Produccion

---
+++ server/classes/ticket-control.js
@@ -0,0 +1,131 @@
+const fs = require('fs');
+
+class Ticket {
+
+    constructor(numero, escritorio) {
+        this.numero = numero;
+        this.escritorio = escritorio;
+    }
+}
+
+class TicketControl {
+
+    constructor() {
+        this.ultimo = 0;
+        this.hoy = new Date().getDate();
+        this.tickets = [];
+        this.ultimos4 = [];
+
+        let data = require('../data/data.json');
+
+        if (data.hoy === this.hoy) {
+
+            this.ultimo = data.ultimo;
+            this.tickets = data.tickets;
+            this.ultimos4 = data.ultimos4;
+
+        } else {
+            this.reiniciarConteo();
+
+        }
+    }
+
+    siguienteTicket() {
+
+        this.ultimo += 1;
+
+        let ticket = new Ticket(this.ultimo, null);
+        this.tickets.push(ticket);
+
+        this.grabarArchivo();
+
+        return `Ticket ${ this.ultimo }`;
+    }
+
+    getUltimoTicket() {
+        return `Ticket ${ this.ultimo }`;
+
+    }
+
+    getUltimos4() {
+        return this.ultimos4;
+
+    }
+
+    atenderTicket(escritorio) {
+        if (this.tickets.length === 0) {
+            return 'No hay tickets sin atender';
+        }
+
+        let numeroTicket = this.tickets[0].numero;
+        this.tickets.shift();
+
+        let atenderTicket = new Ticket(numeroTicket, escritorio);
+
+        this.ultimos4.unshift(atenderTicket);
+
+        /**
+         * En pantalla vamos a tener cuatro tickets a la vista, entonces
+         * cuando el número de tickest sea mayor que 4 habrá que borrar 
+         * el último
+         */
+
+        if (this.ultimos4.length > 4) {
+            this.ultimos4.splice(-1, 1);
+        }
+
+        this.grabarArchivo();
+
+        return atenderTicket;
+
+
+    }
+
+    /**
+     * Creamos una funcion para reiniciar los datos
+     */
+
+    reiniciarConteo() {
+
+        this.ultimo = 0;
+        this.tickets = [];
+        this.ultimos4 = [];
+
+        console.log("Se ha reiniciado el sistema");
+        this.grabarArchivo();
+
+    }
+
+    grabarArchivo() {
+        /**
+         * Recogemos los datos para el reinicio. La variable debe
+         * tener la misma estructura que tiene el json
+         */
+
+        let jsonData = {
+            ultimo: this.ultimo,
+            hoy: this.hoy,
+            tickets: this.tickets,
+            ultimos4: this.ultimos4
+        }
+
+        /**
+         * Lo convertimos a un string para grabarlo en el json
+         */
+
+        let jsonDataString = JSON.stringify(jsonData);
+
+        /**
+         * Lo grabamos en el archivo json. Para ello tenemos que importar
+         * el Filesystem
+         */
+        fs.writeFileSync('./server/data/data.json', jsonDataString);
+
+    }
+
+}
+
+
+module.exports = {
+    TicketControl
+}
